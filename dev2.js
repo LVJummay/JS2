dfae11ad14e0b25fdf7fb0e9c1e90a6023aa8d84
lvjummay <lvjummay@gmail.com>
2024-11-25 20:57:45 -0500

    first commit

---
+++ dev2.js
@@ -0,0 +1,35 @@
+let a = 10;
+let b = 20;
+const sumab = a + b;
+console.log(sumab);
+const difab = a - b;
+console.log(difab);
+const multab = a * b;
+console.log(multab);
+const divab = a / b;
+console.log(divab);
+let firstName = "Vladimy"
+let lastName = "Laurore"
+const fullName = firstName + " " + lastName
+console.log(fullName);
+let person = {
+    name: "Vladimy",
+    age: 40,
+    hoby: "swiming"
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.hoby);
+const favoriteFoods= ["mangoes" , "rice" , "Pasta" , "Cassava" , "nut"];
+console.log(favoriteFoods)
+console.log(favoriteFoods[0])
+console.log(favoriteFoods[favoriteFoods.length - 1]);
+let car = {
+    brand: "Mercedes",
+    model: "E-Class",
+    year: 2023,
+    features: ["Adaptive Cruise Control", "MBUX Infotainment System", "Panoramic Sunroof"]
+}
+console.log(car);
+
+console.log(car.features[0]); 
